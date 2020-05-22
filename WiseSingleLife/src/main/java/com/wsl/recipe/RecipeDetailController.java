a2e5975d41e6b1a8c19f9f384e323a72383df5db
Sujin Lee <sjnu21@naver.com>
2020-05-22 16:30:36 +0900

    레시피 디테일 페이지

---
+++ WiseSingleLife/src/main/java/com/wsl/recipe/RecipeDetailController.java
@@ -0,0 +1,20 @@
+package com.wsl.recipe;
+
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RecipeDetailController {
+	
+	@Autowired
+	private RecipeDAO dao;
+	
+	@RequestMapping("recipe_detail.do")
+	public String recipe_detail() {
+		
+		return "recipe/detail";
+	}
+}
