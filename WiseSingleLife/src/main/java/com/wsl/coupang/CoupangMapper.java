85d0d02c9b49c38f85099d2e5c2bcf789eaa9521
hajieun1218 <gkwldms1218@naver.com>
2020-05-26 11:03:54 +0900

    json 테스트

--- WiseSingleLife/src/main/java/com/wsl/coupang/CoupangMapper.java
+++ WiseSingleLife/src/main/java/com/wsl/coupang/CoupangMapper.java
@@ -3,6 +3,7 @@ package com.wsl.coupang;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 
+import com.wsl.emart.EmartVO;
 import com.wsl.search.SearchKeywordVO;
 import java.util.*;
 
@@ -30,5 +31,16 @@ public interface CoupangMapper {
 	
 	@Insert("INSERT INTO keyword_mart_mapper VALUES(#{codeNo},#{productCode},#{rank})")
 	public void keywordMartMapperInsert(CoupangVO vo);
+	
+	
+	@Select("SELECT a.rank, b.* FROM keyword_mart_mapper a, coupang b"
+			+ " WHERE a.codeno=#{codeno} AND a.productcode=b.productcode"
+			+ " ORDER BY rank ASC")
+	public List<CoupangVO> getCoupangVOListByCodeNo(int codeNo);
+	
+	@Select("SELECT a.*, RANK() OVER(ORDER BY score DESC, a.price ASC) AS rank"
+			+ " FROM (SELECT coupang.*, rate*log(10,reviewcount+1) AS score"
+			+ " FROM coupang WHERE name LIKE '%'||#{keyword}||'%') a")
+	public List<CoupangVO> getCoupangVOListByKeyword(String keyword);
 		
 }
--- WiseSingleLife/src/main/java/com/wsl/emart/EmartMapper.java
+++ WiseSingleLife/src/main/java/com/wsl/emart/EmartMapper.java
@@ -3,6 +3,8 @@ package com.wsl.emart;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 
+import com.wsl.lottemart.LotteMartVO;
+
 import java.util.*;
 
 public interface EmartMapper {
@@ -22,29 +24,33 @@ public interface EmartMapper {
 	
 	//  ===================================================================
 	
-	@Select("SELECT img,name,price,unitprice,num "
+	/*@Select("SELECT img,name,price,unitprice,num "
 		   +"FROM (SELECT img,name,price,unitprice,rownum as num "
 		   +"FROM emart WHERE name LIKE '%'||#{keyword}||'%') "
 		   +"WHERE num BETWEEN 1 AND 3")
-	public List<EmartVO> emartSearchData(String keyword); 
+	public List<EmartVO> emartSearchData(String keyword);*/ 
 	
-	/*
-	 * SELECT * FROM emart,
-(SELECT * FROM keyword_mart_mapper
-WHERE codeno=116 AND productcode LIKE 'em%'
-AND rank<=10
-ORDER BY rank) km
-WHERE emart.productcode=km.productcode;
-	 */
-	@Select("SELECT * FROM emart, "
+	
+	/*@Select("SELECT * FROM emart, "
 			+ "(SELECT * FROM keyword_mart_mapper "
 			+ "WHERE codeno=#{codeno} AND productcode LIKE 'em%' "
 			+ "AND rank<=10 "
 			+ "ORDER BY rank) km "
 			+ "WHERE emart.productcode=km.productcode")
-	public List<EmartVO> emartSearchData2(int codeno);
+	public List<EmartVO> emartSearchData2(int codeno);*/
 	
 	@Insert("INSERT INTO keyword_mart_mapper VALUES(#{codeno},#{productcode},#{rank})")
 	public void keywordMartMapperInsert(EmartVO vo);
+	
+	
+	@Select("SELECT a.rank, b.* FROM keyword_mart_mapper a, emart b"
+			+ " WHERE a.codeno=#{codeno} AND a.productcode=b.productcode"
+			+ " ORDER BY rank ASC")
+	public List<EmartVO> getEmartVOListByCodeNo(int codeNo);
+	
+	@Select("SELECT a.*, RANK() OVER(ORDER BY score DESC, a.price ASC) AS rank"
+			+ " FROM (SELECT emart.*, rate*log(10,reviewcount+1) AS score"
+			+ " FROM emart WHERE name LIKE '%'||#{keyword}||'%') a")
+	public List<EmartVO> getEmartVOListByKeyword(String keyword);
 	
 }
