96e1784159a118aa7f52658048b0f1ca249d3a59
parkchanhwi <pcj9501@naver.com>
2020-05-27 16:08:17 +0900

    게시판 수정중

--- WiseSingleLife/src/main/java/com/wsl/board/BoardMapper.java
+++ WiseSingleLife/src/main/java/com/wsl/board/BoardMapper.java
@@ -2,7 +2,11 @@ package com.wsl.board;
 
 import java.util.*;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
+import org.apache.ibatis.annotations.Update;
 
 public interface BoardMapper {
 	@Select("SELECT no,author,title,content,regdate,hit,num "
@@ -14,4 +18,35 @@ public interface BoardMapper {
 	
 	@Select("SELECT CEIL (COUNT(*)/15.0) FROM board")
 	public int boardTotalPage();
+	
+	@SelectKey(keyProperty="no",resultType=int.class,before=true,
+		statement="SELECT NVL(MAX(no)+1,1) as no FROM board")
+	@Insert("INSERT INTO board(no,title,author,content,pwd) VALUES("
+			+ "#{no},#{title},#{author},#{content},#{pwd})")
+	public void boardInsert(BoardVO vo);
+	
+	@Select("SELECT * FROM board "
+			+ "WHERE no=#{no}")
+	public BoardVO boardDetailData(int no);
+	
+	@Update("UPDATE board SET "
+			+ "hit=hit+1 "
+			+ "WHERE no=#{no}")
+	public void boardHitIncrement(int no);
+	
+	@Select("SELECT pwd FROM board "
+			+ "WHERE no=#{no}")
+	public String boardGetPwd(int no);
+	
+	@Update("UPDATE board SET "
+			+ "author=#{author},title=#{title},content=#{content} "
+			+ "WHERE no=#{no}")
+	public void boardUpdate(BoardVO vo);
+	/*
+	 * DELETE FROM replyBoard
+		WHERE bno=#{bno}
+	 */
+	@Delete("DELETE FROM board "
+			+ "WHERE no=#{no}")
+	public void boardDelete(int no);
 }
