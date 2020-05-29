9d59376c76f82cadc62e47b16fa1847d1441df13
hajieun1218 <gkwldms1218@naver.com>
2020-05-29 18:48:24 +0900

    mongodb test

---
+++ WiseSingleLife/src/main/java/com/wsl/mongo/SearchVO.java
@@ -0,0 +1,14 @@
+package com.wsl.mongo;
+
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SearchVO {
+	private int no;
+	private String keyword;
+	private String regdate;
+}
